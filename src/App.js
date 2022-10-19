47d5713bacbc13bb0927c68319f1c86b72ff01f8
José <josevenancioliveira@gmail.com>
2022-10-19 14:30:11 -0300

    added logic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,98 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Button from '@mui/material/Button';
+import { AppBar, Grid, TextField, Typography } from '@mui/material';
 import './App.css';
+import { Box } from '@mui/system';
 
 function App() {
+  const [vcc, setVcc] = useState();
+  const [vbb, setVbb] = useState();
+  const [rb, setRb] = useState();
+  const [rc, setRc] = useState();
+  const [gain, setGain] = useState();
+  const [showResult, setShowResult] = useState(false);
+
+  const circuit = "CQAgjCAMB0l3BWcMBsBmALAJhQdgWgJwAcYKCFIGaIWICk9ApgLRhgBQA7iGpBrVx0+AtCmLhuvfrQyMR4QnU4BzaaPHrexAY0gcALoLpYEKLafOMILNNDEYMxLGGfFCGFGDNRoKLI64hLjOwShehEhgcCAAJkwAZgCGAK4ANgZSAYyWxrQ+nABO4FgSudn5VlS48PoAblRkvJoYTWBKUJ1y4Eh6vggcDWClzRLDZT593d6detADxa3m7cIyK51icFJLo1piEvo8CvslEif6QA";
+  const falstad = `http://www.falstad.com/circuit/circuitjs.html?hideSidebar=true&hideMenu=true&ctz=${circuit}&whiteBackground=true`;
+
+
+  const getValue = (label, fn) => {
+    return (
+      <Grid item xs={3} className='input-textfield'>
+        <TextField
+          type="number"
+          label={label}
+          onChange={(event) => {
+            fn(event.target.value);
+          }} />
+
+      </Grid>
+    )
+  }
+
+  const renderResult = () => {
+    const ib = (vbb - 0.7) / rb;
+    const ic = ib * gain;
+    const vce = vcc - (rc * ic);
+    return (
+      <div>
+
+        <div className='gride'>
+          <Grid container className='gride'>
+            <Typography>
+              Ib = {ib} A
+            </Typography>
+          </Grid>
+          <Grid container className='gride'>
+            <Typography>
+              Ic = {ic} A
+            </Typography>
+          </Grid>
+          <Grid container className='gride'>
+            <Typography>
+              Vce = {vce} V
+            </Typography>
+          </Grid>
+        </div>
+        <iframe src={falstad} title="falstad" className='falstad'></iframe>
+      </div>
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <Box sx={{ flexGrow: 1 }} className='head'>
+        <AppBar position="fixed">
+          <Typography
+            variant="h6"
+            align='center'
+            sx={{ flexGrow: 1}}
+            className='head'>
+            Calculadora de Transistores
+          </Typography>
+        </AppBar>
+      </Box>
+
+      <Grid container className="grida">
+
+        {getValue("Vcc", setVcc)}
+        {getValue("Vbb", setVbb)}
+        {getValue("Rb", setRb)}
+        {getValue("Rc", setRc)}
+        {getValue("Ganho", setGain)}
+
+      </Grid>
+      <Grid container className='grido'>
+        <Button
+          variant="contained"
+          disabled={vcc && vbb && rc && rb && gain ? false : true}
+          onClick={() => { setShowResult(true) }}>
+          Calcular
+        </Button>
+      </Grid>
+      {showResult && (renderResult())}
+
+    </div >
   );
 }
 
